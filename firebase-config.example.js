2db58bb9537b7615098adb48bcfceb7c903fa8d2
AET Technology <aettechnology5@gmail.com>
2026-08-21 17:59:16 +0000

    Add firebase-config.example.js

---
+++ firebase-config.example.js
@@ -0,0 +1,5 @@
+window.AET_FIREBASE_CONFIG={
+  apiKey:"AIzaSy...",authDomain:"aet-rencontre.firebaseapp.com",
+  projectId:"aet-rencontre",storageBucket:"aet-rencontre.appspot.com",
+  messagingSenderId:"1234567890",appId:"1:1234567890:web:abcdef123456",
+  measurementId:"G-XXXXXXXXXX"};
