e8738a9a0249a77f00d1686302901da3d210099b
AET Technology <aettechnology5@gmail.com>
2026-08-22 13:08:33 +0000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,12 @@
-/* AET RENCONTRE - remplacer les valeurs VOTRE_*_ICI par votre config Firebase */
+/* AET RENCONTRE - configuration Firebase reelle du projet aet-rencontre */
 window.AET_FIREBASE_CONFIG={
-  apiKey:"VOTRE_API_KEY_ICI",
+  apiKey:"AIzaSyASUwrt0mOpA5uzgYuEkydaQnHr7HZixkQ",
   authDomain:"aet-rencontre.firebaseapp.com",
+  databaseURL:"https://aet-rencontre-default-rtdb.firebaseio.com",
   projectId:"aet-rencontre",
-  storageBucket:"aet-rencontre.appspot.com",
-  messagingSenderId:"000000000000",
-  appId:"VOTRE_APP_ID_ICI",
-  measurementId:"VOTRE_MEASUREMENT_ID_ICI"};
+  storageBucket:"aet-rencontre.firebasestorage.app",
+  messagingSenderId:"765485847764",
+  appId:"1:765485847764:web:4faff6c45eb07736bcd741",
+  measurementId:"G-0K4R4QXSTG"};
 window.AET_USE_FIREBASE=!window.AET_FIREBASE_CONFIG.apiKey.startsWith("VOTRE_");
 window.AET_DEMO_MODE=!window.AET_USE_FIREBASE;
